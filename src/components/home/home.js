196dc29b35eb76279eef6c51778b405b7eb5dfa9
ebiz <lustriaebiz@gmail.com>
2018-01-06 18:08:53 +0700

    add Thumbnail

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
+import { Thumbnail } from 'native-base';
 
 export default class Home extends Component {
   constructor(props) {
@@ -12,11 +13,13 @@ export default class Home extends Component {
 
     return (
       <View style={styles.container}>
-        <Text>Welcome to Cek Ongkir App</Text>
-        <Text></Text>
+      <Thumbnail square  large source={{uri: 'https://utekno.com/wp-content/uploads/2015/11/logo-tiki.png'}} />
+      <Thumbnail large source={{uri: 'https://i.pinimg.com/originals/8d/3b/f8/8d3bf8fda0c800bde077815241705bf3.png'}} />
+      <Thumbnail large source={{uri: 'https://upload.wikimedia.org/wikipedia/en/thumb/1/16/Pos_Indonesia_logo.svg/1280px-Pos_Indonesia_logo.svg.png'}} />
+        <Text/><Text/>
         <Button
           onPress={() => navigate('CekOngkir')}
-          title="Next"
+          title="Cek Ongkir"
         />
       </View>
     );
